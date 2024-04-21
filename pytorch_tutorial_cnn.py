578867bfece159ed623584b76025df1a8cbd8350
Dipsht13 <alec.mudek@yahoo.com>
2024-04-21 12:16:15 -0400

    Finished CNN tutorial.

--- pytorch_tutorial_cnn.py
+++ pytorch_tutorial_cnn.py
@@ -17,6 +17,9 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
+import time
+start = time.time()
+
 
 # need to convert MNIST image files into 4-d tensors (# of images, height, width, color)
 transform = transforms.ToTensor()
@@ -135,6 +138,126 @@ optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 
 # now cue the training montage music
 
+# need to store some shit
+train_losses = []
+train_correct = []
+
+test_losses = []
+test_correct = []
+
+# loop through epochs
+epochs = 5
+for epoch in range(epochs):
+    trn_corr = 0 # running total of correct results in training
+    tst_corr = 0 # ^         what he said            ^ testing
+    
+    # train
+    for b, (x_train, y_train) in enumerate(train_loader):
+        b += 1 #start batches at 1
         
+        y_pred = model(x_train) # get predicted values from training set
+        loss = criterion(y_pred, y_train) # you know what it is bitch
         
+        predicted = torch.max(y_pred.data, 1)[1] # add up the number of correct predictions; indexed off the 1st point
+        # NOTE: Adding the 1 in the torch.max(y_pred.data, 1) results in more
+        #       feedback from the torch.max() function. If you just say 
+        #       torch.max(y_pred.data) it will return the max value in the 
+        #       tensor. Saying torch.max(y_pred.data, 1) returns both the 
+        #       max value AND that value's index. Since we care about which
+        #       index contains the max value, we pull the [1] index out of the
+        #       output.
+        batch_corr = (predicted == y_train).sum() # how many we got correct from this batch
+        trn_corr += batch_corr # update the total number of correct in training
         
+        # now update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # report progress
+        if b%600 == 0:
+            print('Epoch: ' + str(epoch), end = '; ')
+            print('Batch: ' + str(b), end = '; ')
+            print('Loss: ' + str(loss.item()))
+            
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+
+    # now test it (still in the epoch loop, want to see how this changes over time)
+    with torch.no_grad():
+        for b, (x_test, y_test) in enumerate(test_loader):
+            y_val = model(x_test)
+            predicted = torch.max(y_val.data, 1)[1]
+            tst_corr += (predicted == y_test).sum()
+            
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)       
+
+print('Training Time: ' + str(round((time.time() - start)/60, 2)) + ' minutes.')
+
+# now plot shit
+train_losses = [tl.item() for tl in train_losses]
+
+plt.plot(train_losses, label = 'Training Loss')
+plt.plot(test_losses, label = 'Validation Loss')
+plt.title('Loss at Epoch')
+plt.legend()
+
+plt.figure()
+plt.plot([t/600 for t in train_correct], label = 'Training Accuracy')
+plt.plot([t/100 for t in test_correct], label = 'Validation Accuracy')
+plt.title('Accuracy at the End of Each Epoch')
+plt.legend()
+
+# load the test data with batch_size == number of images (why? idk)
+test_load_everything = DataLoader(test_data, batch_size = 10000, shuffle = False)
+
+with torch.no_grad():
+    correct = 0
+    for x_test, y_test in test_load_everything:
+        y_val = model(x_test)
+        predicted = torch.max(y_val, 1)[1]
+        correct += (predicted == y_test).sum()
+
+# how many did it get right?
+correct.item()/len(test_data) # 98.72% correct
+
+
+# now try running an image through the model
+test_data[4143] # it's a 9
+# grab just the data
+test_data[4143][0]
+
+# need to reshape it
+test_data[4143][0].reshape(28, 28) #originally of shape (1, 28, 28); don't need the first dim
+
+#show the image
+plt.figure()
+plt.imshow(test_data[4143][0].reshape(28, 28))
+
+# now pass that 9 through our model
+model.eval()
+with torch.no_grad():
+    new_prediction = model(test_data[4143][0].view(1, 1, 28, 28)) # batch size of 1, 1 color channel, 28x28 image
+    
+cnn_output = torch.max(new_prediction, 1)[1] # it's a 9!!
+
+
+# save the network for future reference
+torch.save(model.state_dict(), 'saved_nns/first_tutorial_cnn.pt')
+
+#to load the model in later start by initializing a blank one
+model_reloaded = ConvolutionalNetwork()
+
+#saving/loading models is done through the internal state dict
+model_reloaded.load_state_dict(torch.load('saved_nns/first_tutorial_cnn.pt'))
+
+#can verify that everything loaded in correctly with
+model_reloaded.eval()
+with torch.no_grad():
+    newer_prediction = model_reloaded(test_data[4143][0].view(1, 1, 28, 28)) # batch size of 1, 1 color channel, 28x28 image
+    
+cnn_reloaded_output = torch.max(newer_prediction, 1)[1] # it's still a 9!!
+
+    
